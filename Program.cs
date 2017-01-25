ca385b63a4911367e96cf715622661527dee1035
TaHuC <TaHuC@hotmail.com>
2017-01-25 21:59:51 +0200

    Finish

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RefactorVolumeOfPiramid
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double lenght, width, height = 0;
+
+			Console.Write("Length: ");
+
+			lenght = double.Parse(Console.ReadLine());
+
+			Console.Write("Width: ");
+
+			width = double.Parse(Console.ReadLine());
+
+			Console.Write("Height: ");
+
+			height = double.Parse(Console.ReadLine());
+
+			height = (lenght * width * height) / 3;
+
+			Console.WriteLine("Pyramid Volume: {0:F2}", height);
+		}
+	}
+}
